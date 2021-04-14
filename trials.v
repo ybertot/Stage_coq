0957ca7e293fa3fea14bd2e786ea8638e0debf47
Yves Bertot <Yves.Bertot@inria.fr>
2021-04-14 17:27:13 +0200

    This version of trials has a correct display_cell function, examples have been removed, they should be in another file

--- trials.v
+++ trials.v
@@ -105,267 +105,27 @@ Fixpoint iterate_cell_border (scale : string) (f : string -> pt -> pt -> string 
   match s with
   | nil => buffer
   | p :: nil => f scale p last buffer
-  | p1 :: (p2 :: _ as tl) =>
+  | p1 :: (p2 :: _) as tl =>
     let buffer' := iterate_cell_border scale f last tl buffer in
     f scale p1 p2 buffer'
   end.
 
 Definition display_cell_border scale s buffer :=
   match s with
-  | a :: tl => iterate_cell_border scale display_segment a tl buffer
+  | a :: tl => iterate_cell_border scale display_segment a s buffer
   | nil => buffer
   end.
 
 Definition display_cell scale (c : cell) (buf : string) : string :=
   display_cell_border scale (pts c) buf.
 
-(* Example with a single triangle. *)
-
-Definition one_triangle :=
-  ([:: 2; 2; 3; 5; 2; 2; 4; 4; 3; 5; 4; 4])%N.
-
-Definition two_triangles :=
-  ([:: 1; 2; 5; 1; 5; 1; 6; 17; 1; 2; 6; 17;
-      2; 2; 3; 5; 2; 2; 4; 4; 3; 5; 4; 4])%N.
-
-(* The result of the next command should be 2. *)
-Compute if test one_triangle is Some v then size v else 0%N.
-Compute test one_triangle.
-
-Compute if test two_triangles is Some v then size v else 0%N.
-Compute test two_triangles.
-
-Definition cell0 := {| pts := [::]; low := Bedge (isT : p_x {| p_x := 0; p_y := 1|} < p_x {| p_x := 1; p_y := 1|}); high := Bedge (isT : p_x {| p_x := 0; p_y := 1|} < p_x {| p_x := 1; p_y := 1|})|}.
-Compute if test two_triangles is Some v then Some (nth cell0 v 6) else None.
-
-Definition expected_result :=
-  [:: {| pts :=
-          [:: {|p_x := 6%:R; p_y := 17%:R |};
-              {|p_x := 5%:R; p_y := 14%:R |};
-              {|p_x := 5%:R; p_y := 1%:R |}];
-          low :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 5%:R; p_y := 1%:R |} <
-                   p_x {| p_x := 6%:R; p_y := 17%:R |}));
-          high :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 1%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 6%:R; p_y := 17%:R |}))
-          |};
-      {| pts :=
-          [:: {|p_x := 5%:R; p_y := 1%:R |};
-              {|p_x := 5%:R; p_y := 14%:R |};
-              {|p_x := 4%:R; p_y := 11%:R |};
-              {|p_x := 4%:R; p_y := (5%:R / 4%:R) |}];
-          low :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 1%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 5%:R; p_y := 1%:R |}));
-          high :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 1%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 6%:R; p_y := 17%:R |}))
-          |};
-     {| pts :=
-          [:: {|p_x := 4%:R; p_y := (5%:R / 4%:R) |};
-              {|p_x := 4%:R; p_y := 4%:R |};
-              {|p_x := 2%:R; p_y := 2%:R |};
-              {|p_x := 2%:R; p_y := (7%:R / 4%:R) |}];
-          low :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 1%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 5%:R; p_y := 1%:R |}));
-          high :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 2%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 4%:R; p_y := 4%:R |}))
-          |};
-     {| pts :=
-          [:: {|p_x := 4%:R; p_y := 4%:R |};
-              {|p_x := 3%:R; p_y := 5%:R |};
-              {|p_x := 3%:R; p_y := 3%:R |}];
-          low :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 2%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 4%:R; p_y := 4%:R |}));
-          high :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 3%:R; p_y := 5%:R |} <
-                   p_x {| p_x := 4%:R; p_y := 4%:R |}))
-          |};
-     {| pts :=
-          [:: {|p_x := 4%:R; p_y := 4%:R |};
-              {|p_x := 4%:R; p_y := 11%:R |};
-              {|p_x := 3%:R; p_y := 8%:R |};
-              {|p_x := 3%:R; p_y := 5%:R |}];
-          low :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 3%:R; p_y := 5%:R |} <
-                   p_x {| p_x := 4%:R; p_y := 4%:R |}));
-          high :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 1%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 6%:R; p_y := 17%:R |}))
-          |};
-     {| pts :=
-          [:: {|p_x := 3%:R; p_y := 3%:R |};
-              {|p_x := 3%:R; p_y := 5%:R |};
-              {|p_x := 2%:R; p_y := 2%:R |}];
-          low :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 2%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 4%:R; p_y := 4%:R |}));
-          high :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 2%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 3%:R; p_y := 5%:R |}));
-          |};
-     {| pts :=
-          [:: {|p_x := 3%:R; p_y := 5%:R |};
-              {|p_x := 3%:R; p_y := 8%:R |};
-              {|p_x := 2%:R; p_y := 5%:R |};
-              {|p_x := 2%:R; p_y := 2%:R |}];
-          low :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 2%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 3%:R; p_y := 5%:R |}));
-          high :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 1%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 6%:R; p_y := 17%:R |}));
-
-          |};
-     {| pts :=
-          [:: {|p_x := 2%:R; p_y := (7%:R / 4%:R) |};
-              {|p_x := 2%:R; p_y := 5%:R |};
-              {|p_x := 1%:R; p_y := 2%:R |}];
-          low :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 1%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 5%:R; p_y := 1%:R |}));
-          high :=
-            Bedge
-               (erefl
-                  (p_x {| p_x := 1%:R; p_y := 2%:R |} <
-                   p_x {| p_x := 6%:R; p_y := 17%:R |}))
-          |}].
-
-Compute (concat newline ([:: "%!PS"; "100 100 translate newpath ";
-    (display_cell (cats "20 mul " newline)
-         (nth cell0 expected_result 0)
-                 (cats "stroke showpage" newline))])%string).
-
-Compute  (concat newline ([:: "%!PS"; "100 100 translate newpath ";
-    (foldr (display_cell (cats "20 mul " newline))
-                 (cats "stroke showpage" newline) expected_result)])%string)
-
-.
-
-
-Compute (* map (fun c => display_cell "50 mul " c "") *) (start [::
-     Bevent {| p_x := 2; p_y := 2 |}
-        [::]
-        [:: [edge of {| p_x := 2; p_y := 2 |},
-                     {| p_x := 4; p_y := 4 |}];
-            [edge of {| p_x := 2; p_y := 2 |},
-                     {| p_x := 3; p_y := 5 |}]];
-     Bevent {| p_x := 3%:R; p_y := 5%:R |}
-        [:: [edge of {| p_x := 2; p_y := 2%:R |},
-                {| p_x := 3%:R; p_y := 5%:R|}]]
-        [:: [edge of {| p_x := 3%:R; p_y := 5%:R|},
-                {| p_x := 4%:R; p_y := 4%:R|}]];
-    Bevent {| p_x := 4%:R; p_y := 4%:R |}
-      [:: [edge of {| p_x := 2%:R; p_y := 2%:R |},
-              {| p_x := 4%:R; p_y := 4%:R |}];
-          [edge of {| p_x := 3%:R; p_y := 5%:R |},
-              {| p_x := 4%:R; p_y := 4%:R |}]] [::]]).
-
-
-
-Compute (* map (fun c => display_cell "50 mul " c "") *) (start [::
-     Bevent {| p_x := 1; p_y := 2 |} [::]
-        [:: [edge of {| p_x := 1; p_y := 2 |},
-                     {| p_x := 5; p_y := 1 |}];
-            [edge of {| p_x := 1; p_y := 2 |},
-                     {| p_x := 6; p_y := 17 |}]];
-     Bevent {| p_x := 2; p_y := 2 |}
-        [::]
-        [:: [edge of {| p_x := 2; p_y := 2 |},
-                     {| p_x := 4; p_y := 4 |}];
-            [edge of {| p_x := 2; p_y := 2 |},
-                     {| p_x := 3; p_y := 5 |}]];
-     Bevent {| p_x := 3%:R; p_y := 5%:R |}
-        [:: [edge of {| p_x := 2; p_y := 2%:R |},
-                {| p_x := 3%:R; p_y := 5%:R|}]]
-        [:: [edge of {| p_x := 3%:R; p_y := 5%:R|},
-                {| p_x := 4%:R; p_y := 4%:R|}]];
-    Bevent {| p_x := 4%:R; p_y := 4%:R |}
-      [:: [edge of {| p_x := 2%:R; p_y := 2%:R |},
-              {| p_x := 4%:R; p_y := 4%:R |}];
-          [edge of {| p_x := 3%:R; p_y := 5%:R |},
-              {| p_x := 4%:R; p_y := 4%:R |}]] [::];
-    Bevent {| p_x := 5%:R; p_y := 1%:R |}
-      [:: [edge of {| p_x := 1%:R; p_y := 2%:R|},
-                   {| p_x := 5%:R; p_y := 1|}]]
-      [:: [edge of {| p_x := 5; p_y := 1|},
-                   {| p_x := 6; p_y := 17 |}]];
-    Bevent {| p_x := 6; p_y := 17 |}
-      [:: [edge of {| p_x := 5; p_y := 1|},
-              {| p_x := 6; p_y := 17 |}];
-          [edge of {| p_x := 1; p_y := 2|},
-             {|p_x := 6; p_y := 17 |}]] [::]]).
-
-Compute map pts (start [::
-     Bevent {| p_x := 1; p_y := 2 |} [::]
-        [:: [edge of {| p_x := 1; p_y := 2 |},
-                     {| p_x := 5; p_y := 1 |}];
-            [edge of {| p_x := 1; p_y := 2 |},
-                     {| p_x := 6; p_y := 17 |}]];
-     Bevent {| p_x := 2; p_y := 2 |}
-        [::]
-        [:: [edge of {| p_x := 2; p_y := 2 |},
-                     {| p_x := 4; p_y := 4 |}];
-            [edge of {| p_x := 2; p_y := 2 |},
-                     {| p_x := 3; p_y := 5 |}]];
-     Bevent {| p_x := 3%:R; p_y := 5%:R |}
-        [:: [edge of {| p_x := 2; p_y := 2%:R |},
-                {| p_x := 3%:R; p_y := 5%:R|}]]
-        [:: [edge of {| p_x := 3%:R; p_y := 5%:R|},
-                {| p_x := 4%:R; p_y := 4%:R|}]];
-    Bevent {| p_x := 4%:R; p_y := 4%:R |}
-      [:: [edge of {| p_x := 2%:R; p_y := 2%:R |},
-              {| p_x := 4%:R; p_y := 4%:R |}];
-          [edge of {| p_x := 3%:R; p_y := 5%:R |},
-              {| p_x := 4%:R; p_y := 4%:R |}]] [::];
-    Bevent {| p_x := 5%:R; p_y := 1%:R |}
-      [:: [edge of {| p_x := 1%:R; p_y := 2%:R|},
-                   {| p_x := 5%:R; p_y := 1|}]]
-      [:: [edge of {| p_x := 5; p_y := 1|},
-                   {| p_x := 6; p_y := 17 |}]];
-    Bevent {| p_x := 6; p_y := 17 |}
-      [:: [edge of {| p_x := 5; p_y := 1|},
-              {| p_x := 6; p_y := 17 |}];
-          [edge of {| p_x := 1; p_y := 2|},
-             {|p_x := 6; p_y := 17 |}]] [::]]).
-
-Compute vertical_intersection_point {| p_x := 3; p_y := 5|}
-     [edge of {| p_x := 1; p_y := 2 |},
-                     {| p_x := 6; p_y := 17 |}].
-
-Compute pue_formula  {| p_x := 1; p_y := 2|}  {| p_x := 3; p_y := 5|}
-     {| p_x := 6; p_y := 7|}.
+Definition dtest (s : seq nat) :=
+  match test s with
+  | Some v =>
+    concat newline ([:: "%!PS"; "100 100 translate"; "newpath";
+    foldr (display_cell (cats 
+    newline    
+    "20 mul "%string)) 
+    (concat newline [:: "stroke"; "showpage"; newline]) v])%string
+  | None => EmptyString
+  end.
