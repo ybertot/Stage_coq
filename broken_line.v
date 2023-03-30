c26acedafcd64753428b951c6e498e962e06a232
Yves Bertot <yves.bertot@inria.fr>
2023-03-30 21:30:32 +0200

    made an algorithm that builds a path from a sequence of connected cells

--- broken_line.v
+++ broken_line.v
@@ -5,7 +5,7 @@ From mathcomp Require Import all_ssreflect all_algebra.
 Require Export Field.
 
 From cellDecomposition Require Import math_comp_complements points_and_edges
-  cells.
+  cells cells_alg1.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -137,3 +137,94 @@ move=>/Ih; apply.
   by case pfq : prefix => [ | d prefix'].
 by move: hs; rewrite -cats1 map_cat=> /sorted_catW /andP[].
 Qed.
+
+Definition vert_edge_midpoint (ve : vert_edge) : pt :=
+  {|p_x := ve_x ve; p_y := (ve_top ve + ve_bot ve) / 2%R|}.
+
+Definition lr_connected (c1 c2 : cell) : bool :=
+  [seq v | v <- cell_safe_exits_right c1 & v \in cell_safe_exits_left c2] !=
+  [::].
+
+Definition bi_connected c1 c2 :=
+  lr_connected c1 c2 || lr_connected c2 c1.
+
+Definition dummy_vert_edge :=
+  {| ve_x := 0; ve_top := 0; ve_bot := 0|}.
+
+(* In the first naive version, we assume there is only one common
+  vertical edge between two cells.  This maybe wrong if events can be
+  reduced to a single point. *)
+Definition lr_door (c1 c2 : cell) :=
+  head dummy_vert_edge
+     [seq x | x <- cell_safe_exits_right c1 &
+          x \in cell_safe_exits_left c2].
+
+(* Construct a path between p1 and p2, assuming p1 is in the interior
+  c1 and p2 in the interior of c2, c1 and c2 are lr_connected and
+  respecting the fact that the
+  only door from c1 to c2 is the common vertical edge. *)
+Definition lr_connected_path (c1 c2 : cell) (p1 p2 : pt) :
+  seq (pt * pt) :=
+  [:: (p1, vert_edge_midpoint (lr_door c1 c2));
+      (vert_edge_midpoint (lr_door c1 c2), p2)].
+
+(* A first approximation of a path between two points is a path between
+  cells.  This can be defined using the path function of mathcomp. *)
+
+Definition connected_cells (c : cell) (s : seq cell) :=
+  path bi_connected c s.
+
+(* When building a path from c1 to c3 through c2, an easy case is when
+   lr_connected c1 c2 and lr_connected c2 c3, or in the opposite direction
+   lr_connected c3 c2 and lr_connected c2 c1.   Things are less pleasant
+   if lr_connected c1 c2 and lr_connected c3 c2.  In this case, we need
+   to exhibit a point inside c2 to move away from the vertical boundary,
+   because points on the vertical boundary that are not part of the
+   common vertical edges are likely to be collision points with some
+   obstacle. *)
+
+(* To produce a safe point inside a cell, we can take the point in the middle
+  defined. *)
+
+Definition midpoint (p1 p2 : pt) : pt :=
+ {| p_x := (p_x p1 + p_x p2) / 2; p_y := (p_y p1 + p_y p2) / 2|}.
+
+Definition cell_center (c : cell) :=
+  midpoint
+    (midpoint (last dummy_pt (left_pts c)) (head dummy_pt (right_pts c)))
+    (midpoint (head dummy_pt (left_pts c)) (last dummy_pt (right_pts c))).
+
+(* For a path of the form : c1 c2   cn-1 cn, we want to construct a path
+  from the midpoint of the door from c1 to c2 to the midpoint of the door
+  from cn-1 to cn.  The base case is when there are only two cells in
+  the sequence, because in this case the two doors are the same.
+  --- doors are vertical edges.
+  Because the base case happens when there are two cells, we consider
+  a function with 3 arguments, where two cells are the first two of the path. *)
+Fixpoint connected_cells_path (c1 c2 : cell) (tl : seq cell) :=
+  match tl with
+  | nil => nil
+  | c3 :: tl' =>
+    let tail_path := connected_cells_path c2 c3 tl' in
+    if lr_connected c1 c2 && lr_connected c2 c3 then
+       (vert_edge_midpoint (lr_door c1 c2),
+           vert_edge_midpoint (lr_door c2 c3)) ::
+       tail_path else
+    if lr_connected c3 c2 && lr_connected c2 c1 then
+       (vert_edge_midpoint (lr_door c2 c1),
+           vert_edge_midpoint (lr_door c3 c2)) ::
+       tail_path else
+    if lr_connected c1 c2 (* we assume lr_connected c3 c2 *) then
+       [:: (vert_edge_midpoint (lr_door c1 c2), cell_center c2),
+           (cell_center c2, vert_edge_midpoint (lr_door c3 c2)) &
+           tail_path] else
+    [:: (vert_edge_midpoint (lr_door c2 c1), cell_center c2),
+        (cell_center c2, vert_edge_midpoint (lr_door c2 c3)) &
+        tail_path]
+  end.
+
+Definition point_cell (p : pt) (s : seq cell) : option cell :=
+  match [seq c | c <- s & strict_inside_closed p c] with
+  | nil => None
+  | a :: _ => Some a
+  end.
