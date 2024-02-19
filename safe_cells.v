d5251755e41f2941555a41c652a7edd74afad853
Yves Bertot (he) <yves.bertot@inria.fr>
2024-02-19 14:15:37 +0100

    start a sketch of proof

--- safe_cells.v
+++ safe_cells.v
@@ -20,7 +20,11 @@ Notation cell := (@cell R).
 Notation pt := (@pt R).
 Notation edge := (@edge R).
 
-Variables closed open : seq cell.
+Variables closed : seq cell.
+(* The last open cell.  We need to prove that that its top edge is top.
+   Then, coverage will be given for all obstacles by the fact that all
+   edges in obstacles are different from top. *)
+Variable o_cell : cell.
 Variables bottom top : cell.
 Variable obstacles : seq edge.
 Variables points : seq pt.
@@ -30,7 +34,43 @@ Hypothesis obstacles_in :
      <= points}.
 
 Hypothesis disj_closed :  {in closed &, disjoint_closed_cells R}.
-Hypothesis disj_open :  {in open & closed, disjoint_open_closed_cells R}.
-Hypothesis coverage : {in obstacles, forall g, edge_covered g open closed}.
+Hypothesis disj_open :  {in [:: o_cell] & closed, disjoint_open_closed_cells R}.
+Hypothesis coverage : {in obstacles, forall g, edge_covered g [::] closed}.
 Hypothesis covered_points :
-   {in points, forall p, exists2 c, c \in open ++ closed & p \in left_pts c}.
+   {in points, forall p, exists2 c, c \in [:: o_cell] ++
+             closed & p \in left_pts c}.
+Hypothesis closed_ok : {in closed, forall c, closed_cell_side_limit_ok c}.
+Hypothesis noc : {in obstacles &, forall g1 g2, inter_at_ext g1 g2}.
+
+Lemma above_low : {in closed, forall c p, p === high c -> p >>= low c}.
+Admitted.
+
+Lemma safe_cell_interior c p :
+  c \in closed -> p <<< high c -> p >>> low c ->
+  left_limit c < p_x p < right_limit c ->
+  {in obstacles, forall g, ~~ (p === g)}.
+Proof.
+move=> ccl puh pal /andP[] pright pleft g gin; apply/negP=> pong.
+have pinc : inside_closed' p c.
+  by rewrite inside_closed'E (underW puh) pal pright (ltW pleft).
+have [[opc [pccs [pccssub [highs [cpccs [opco lopcq]]]]]] | ] := coverage gin.
+  by [].
+move=> [[ | pc1 pcc] [pccn0 [pcccl [ highs [conn [lpcc rpcc]]]]]].
+  by [].
+have {}lpcc : left_limit pc1 <= p_x p.
+  by move:(pong)=> /andP[] _ /andP[]; rewrite lpcc.
+move: lpcc; rewrite le_eqVlt=> /orP[/eqP onleft | ].
+  (* We probably need to show that every closed cell has a closed
+     neighbor on the left, except for the first one, but in this case
+     p cannot be inside the closed cell c,
+     So there is another cell, in which p is inside_closed', and disjointness
+     should make it possible to conclude. *)
+  admit.
+elim : pcc pc1 pcccl highs conn rpcc {pccn0} => [ | pc2 pcc Ih]
+  pc1 pcccl highs conn rpcc lpcc.
+  (* if pc1 == c, there is a contradiction, because p <<< high c but
+     p === high pc1 *)
+  (* if pc1 != c we need to perform another case analysis.
+        pf p === low pc1, then we p must be an event, and there exist
+        another cell c' such that inside_closed' p  c' and p_x p = left_limit c'
+        and c' != c, because  left_limit c <   beurk. *)
